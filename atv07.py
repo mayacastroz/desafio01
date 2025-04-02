6cc1958640d688ae024be8708cdd5cc3339da35e
mayara <mayaracastrosilva89@gmail.com>
2025-04-02 11:37:16 -0300

    desafio

---
+++ atv07.py
@@ -0,0 +1,6 @@
+combustivel = input (f'qual o tipo de combustivel?" \n G- gasolinan\ E- Etanoln\n')
+litros = float(input(f"quantos litros foi colocado?))
+
+if  combustivel == 'G' or combustivel == 'g' :
+
+ else combustivel == 'E' or combustivel == 'e' :
---
+++ desafio1.py
@@ -0,0 +1,17 @@
+entrada01 = 3
+entrada02 = 14
+hora = entrada01 + entrada02
+
+min01 = 45
+min02 = 20
+minuto = min01 + min02
+
+print(f'hora {hora}')
+print(f'minuto {minuto}')
+
+if minuto >
+
+
+
+
+
