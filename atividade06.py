1e6aa0fe6dce7acad3c0b63603af9575c5da74de
mayacastroz <mayaracastrosilva89@gmail.com>
2025-04-02 11:38:17 -0300

    Delete atividade06.py

--- atividade06.py
+++
@@ -1,7 +0,0 @@
-nome = input("qual é o seu nome")
-idade = int(input("qual é a sua idade"))
-salario = float(input("qual é o seu salario"))
-
-print(f"meu nome é {nome}")
-print(f"minha idade é {idade}")
-print (f"meu salário é {salario}")
